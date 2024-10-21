820120120a67c7c69933d398d43abdffbe486cd0
matheusinfingardi <matheus.infingardicr@gmail.com>
2024-10-22 00:56:42 +0100

    espacamento

--- src/app/nova-missao/page.tsx
+++ src/app/nova-missao/page.tsx
@@ -1,11 +1,10 @@
-
 import DroneDeliveryForm from '../../components/forms/DroneDeliveryForm';
 import InfoCard from '../../components/info/InfoCard';
 
 export default function DroneDeliveryPage() {
   return (
     <div className="flex flex-col mb-10 md:flex-row p-6">
-      <div className="w-full md:w-1/2 md:mr-14">
+      <div className="w-full md:w-1/2 md:mr-14 mb-10 md:mb-0">
         <DroneDeliveryForm />
       </div>
       <div className="w-full md:w-2/3">
